c82987a1532b3a849ee4c2943010f952795951b4
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2023-08-06 18:00:19 +0600

    feat: update template engine & partials

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -5,4 +5,15 @@ declare(strict_types=1);
 namespace Framework;
 
 class TemplateEngine {
+    public function __construct(private string $basePath) {
+    }
+
+    public function render(string $template, array $data = []) {
+        extract($data, EXTR_OVERWRITE);
+        ob_start();
+        include "{$this->basePath}/{$template}";
+        $output = ob_get_contents();
+        ob_end_clean();
+        return $output;
+    }
 }
