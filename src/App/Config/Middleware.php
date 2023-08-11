ff5cc895709a2438bb42f15bd2c81d528cd4ebdb
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2023-08-11 20:42:30 +0600

    feat: inject errors to template using Flash middleware

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -9,10 +9,12 @@ use App\Middleware\{
     TemplateDataMiddleware,
     ValidationExceptionMiddleware,
     SessionMiddleware,
+    FlashMiddleware,
 };
 
 function registerMiddleware(App $app) {
     $app->addMiddleware(TemplateDataMiddleware::class);
     $app->addMiddleware(ValidationExceptionMiddleware::class);
+    $app->addMiddleware(FlashMiddleware::class);
     $app->addMiddleware(SessionMiddleware::class);
 }
