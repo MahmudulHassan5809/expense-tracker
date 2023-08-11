c8a786ec650cfb959282262c7c971aae0b728781
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2023-08-11 22:01:40 +0600

    feat: create field match  validator an duse it

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use Framework\Rules\{RequiredRule, EmailRule, MinRule, InRule, UrlRule};
+use Framework\Rules\{RequiredRule, EmailRule, MinRule, InRule, UrlRule, MatchRule};
 use Framework\Validator;
 
 class ValidatorService {
@@ -18,6 +18,7 @@ class ValidatorService {
         $this->validator->add('min', new MinRule());
         $this->validator->add('in', new InRule());
         $this->validator->add('url', new UrlRule());
+        $this->validator->add('match', new MatchRule());
     }
 
     public function validateRegister(array $formData) {
@@ -27,7 +28,7 @@ class ValidatorService {
             "country" => ["required", "in:USA,Canada,Mexico"],
             "socialMediaUrl" => ["required", "url"],
             "password" => ["required"],
-            "confirmPassword" => ["required"],
+            "confirmPassword" => ["required", "match:password"],
             "tos" => ["required"],
         ]);
     }
