edb48e9e4b27e6261d8bab3aea2ad11acef3613d
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2023-08-11 20:54:58 +0600

    feat: unset session errors

--- src/App/Middleware/FlashMiddleware.php
+++ src/App/Middleware/FlashMiddleware.php
@@ -14,6 +14,7 @@ class FlashMiddleware implements MiddlewareInterface {
     }
     public function process(callable $next) {
         $this->view->addGlobal("errors", $_SESSION["errors"] ?? []);
+        unset($_SESSION["errors"]);
         $next();
     }
 }
