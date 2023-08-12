f8da1c754d18d27d93f85ac1c776cf20787ffcbb
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2023-08-12 23:21:33 +0600

    feat: update cli.php script using env variable

--- cli.php
+++ cli.php
@@ -1,16 +1,18 @@
 <?php
-
-include __DIR__ .  "/src/Framework/Database.php";
+require __DIR__ . "/vendor/autoload.php";
 
 use Framework\Database;
+use Dotenv\Dotenv;
+use App\Config\Paths;
 
-$db = new Database("pgsql", [
-    "host" => "localhost",
-    "dbname" => "expense-tracker",
-    "port" => "5432"
-], "mahmudul", "152155809");
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
 
-
+$db = new Database($_ENV['DB_DRIVER'], [
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'dbname' => $_ENV['DB_NAME']
+], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 
 $sqlFile = file_get_contents("./database.sql");
 
